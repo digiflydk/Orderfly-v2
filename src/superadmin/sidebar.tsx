42d3da6ed3c391ba1c34a28601dcaf0e5433d0f5
digiflydk <okh@digifly.dk>
2025-09-13 06:50:07 +0200

    Delete src/superadmin directory

--- src/superadmin/sidebar.tsx
+++
@@ -1,8 +0,0 @@
-import { SuperAdminSidebarClient } from "./sidebar-client";
-import { getPlatformBrandingSettings } from "@/app/superadmin/settings/actions";
-
-export async function SuperAdminSidebar() {
-  const brandingSettings = await getPlatformBrandingSettings();
-
-  return <SuperAdminSidebarClient brandingSettings={brandingSettings} />;
-}
