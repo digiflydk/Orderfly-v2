7a13b7e9b1349f82a12f85c90b614f224c1bed35
digiflydk <okh@digifly.dk>
2025-10-20 00:48:38 +0300

    Delete src/app/[brandSlug] directory

--- src/app/[brandSlug]/[locationSlug]/layout.tsx
+++
@@ -1,6 +0,0 @@
-// NEUTRAL LAYOUT – INGEN IMPORTS, INGEN TYPER, INGEN DESTRUKTURERING
-export const runtime = "nodejs";
-
-export default function Layout(props: any) {
-  return <>{props?.children}</>;
-}
