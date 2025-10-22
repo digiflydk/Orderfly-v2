cb27b0170c4fb735a1ecfd9f330b2cf5a81780bc
Omair Khan <okh2071@gmail.com>
2025-10-22 09:55:45 +0000

    OF-431 Checkout error: When i press the button proceed to checkout i get

--- src/app/[brandSlug]/[locationSlug]/checkout/page.tsx
+++ src/app/[brandSlug]/[locationSlug]/checkout/page.tsx
@@ -6,11 +6,12 @@ import { getActiveLocationBySlug } from "@/app/superadmin/locations/actions";
 
 export default async function CheckoutPage({
   params,
-}: { params: { brandSlug: string; locationSlug: string } }) {
-  const brand = await getBrandBySlug(params.brandSlug);
+}: { params: Promise<{ brandSlug: string; locationSlug: string }> }) {
+  const { brandSlug, locationSlug } = await params;
+  const brand = await getBrandBySlug(brandSlug);
   if (!brand) notFound();
 
-  const location = await getActiveLocationBySlug(brand.id, params.locationSlug);
+  const location = await getActiveLocationBySlug(brand.id, locationSlug);
   if (!location) {
     redirect(`/${brand.slug}`);
   }
