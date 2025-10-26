7fd05ad3da7b30072393120152b0cf4d437f1ac5
Omair Khan <okh2071@gmail.com>
2025-10-26 13:15:49 +0000

    OF-449 new error "Running "npm exec --prefix /layers/google.nodejs.fireb

--- src/app/[brandSlug]/checkout/page.tsx
+++ src/app/[brandSlug]/checkout/page.tsx
@@ -3,12 +3,14 @@ import { redirect, notFound } from "next/navigation";
 import { getBrandBySlug } from "@/app/superadmin/brands/actions";
 import { cookies } from "next/headers";
 
-export default async function LegacyCheckoutPage({ params }: { params: { brandSlug: string } }) {
-  const brand = await getBrandBySlug(params.brandSlug);
+export default async function LegacyCheckoutPage({ params }: { params: Promise<{ brandSlug: string }> }) {
+  const { brandSlug } = await params;
+  const brand = await getBrandBySlug(brandSlug);
   if (!brand) notFound();
 
   // If a comfort cookie exists, try to use it - otherwise send to location selection
-  const cookieSlug = cookies().get("of_location")?.value;
+  const cookieStore = await cookies();
+  const cookieSlug = cookieStore.get("of_location")?.value;
   if (cookieSlug) {
     redirect(`/${brand.slug}/${cookieSlug}/checkout`);
   }
