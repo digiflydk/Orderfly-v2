6148fa18b2366f886cf390eb6d1acceaa9226afe
digiflydk <okh@digifly.dk>
2025-11-01 08:57:09 +0100

    Delete src/app/[brandSlug]/[locationSlug] directory

--- src/app/[brandSlug]/[locationSlug]/checkout/confirmation/confirmation-client.tsx
+++
@@ -1,9 +0,0 @@
-
-"use client";
-
-const ConfirmationClient = () => {
-  return null;
-};
-
-export default ConfirmationClient;
-export { ConfirmationClient };
--- src/app/[brandSlug]/[locationSlug]/checkout/layout.tsx
+++
@@ -1,7 +0,0 @@
-
-// NEUTRAL LAYOUT – INGEN IMPORTS, INGEN TYPER, INGEN DESTRUKTURERING
-export const runtime = "nodejs";
-
-export default function Layout(props: { children: React.ReactNode }) {
-  return <>{props?.children}</>;
-}
