cecb20bf8c0fe3deec871fbfbe5770dafd1438e5
Omair Khan <okh2071@gmail.com>
2025-11-27 10:00:05 +0000

    "522-11-125"   title: "Normalize Superadmin locations pages to AsyncPage

--- src/app/superadmin/locations/edit/[locationId]/page.tsx
+++ src/app/superadmin/locations/edit/[locationId]/page.tsx
@@ -1,13 +1,19 @@
 
-
+import { notFound } from 'next/navigation';
 import { LocationFormPage } from '@/components/superadmin/location-form-page';
 import { getLocationById } from '@/app/superadmin/locations/actions';
 import { getBrands } from '@/app/superadmin/brands/actions';
-import { notFound } from 'next/navigation';
+import type { AsyncPageProps } from "@/types/next-async-props";
+import { resolveParams } from "@/lib/next/resolve-props";
 
-export default async function EditLocationPage({ params }: { params: { locationId: string } }) {
+type SuperadminLocationEditParams = {
+    locationId: string;
+};
+
+export default async function EditLocationPage({ params }: AsyncPageProps<SuperadminLocationEditParams>) {
+    const { locationId } = await resolveParams(params);
     const [location, brands] = await Promise.all([
-        getLocationById(params.locationId),
+        getLocationById(locationId),
         getBrands(),
     ]);
 
